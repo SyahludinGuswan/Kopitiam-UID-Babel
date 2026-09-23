61886aa188357e4d2e48267b94fd5f1ce04aab00
SyahludinGuswan <syahludin.guswan08@gmail.com>
2026-09-23 11:22:00 +0700

    chore: remove unused P2 photo guard

--- kopitiam_backend/ZZZZZZZZZZZZZZZZZZZZZZZZ_P2PhotoPipelineGuard.js
+++
@@ -1,6 +0,0 @@
-// P2 photo pipeline guards. Loaded after legacy handlers by the runtime manifest.
-function p2PhotoUploadRequired_(folderPath, code, base64Data, role) {
-  var uploaded = uploadWoPhoto_(folderPath, code, base64Data, role);
-  if (!uploaded || !uploaded.file || !uploaded.url || !uploaded.name) throw new Error("PHOTO_UPLOAD_FAILED");
-  return uploaded;
-}
